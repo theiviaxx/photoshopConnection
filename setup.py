77a6a6d06b3d14041a93fef151aeb73b7a3e317d
Brett Dixon <theiviaxx@gmail.com>
2014-01-25 15:48:29 -0800

    ade into a better package

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='pyps',
+    description='A pure python implentation for communicating to Adobe Photoshop',
+    long_description='A pure python implentation for communicating to and subscribing to events from Adobe Photoshop',
+    version='0.5',
+    author='Brett Dixon',
+    author_email='theiviaxx@gmail.com',
+    license='MIT',
+    url='https://github.com/theiviaxx/photoshopConnection',
+    platforms='any',
+    include_package_data=True,
+    install_requires=[
+        'pbkdf2'
+    ],
+    packages=[
+        'pyps',
+        'py_des',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    zip_safe=False
+)
