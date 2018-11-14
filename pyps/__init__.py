ab5e3715d3bdc69601bc6dd0f29348f59fa3d612
Brett Dixon <theiviaxx@gmail.com>
2018-11-13 21:08:50 -0800

    bumped version

--- pyps/__init__.py
+++ pyps/__init__.py
@@ -15,7 +15,7 @@ from ._des import des, triple_des
 __title__ = 'pyps'
 __author__ = 'Brett Dixon'
 __email__ = 'theiviaxx@gmail.com'
-__version__ = '0.5.4'
+__version__ = '0.6.0'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015 Brett Dixon'
 
